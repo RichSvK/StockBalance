3a14d2163702b3056a1ea6f164073365aaaca51c
RichSvK <richardsugiharto0@gmail.com>
2025-06-03 23:49:10 +0700

    Initial Commit

---
+++ StockBalance/StockBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockBalanceApp.swift
+//  StockBalance
+//
+//  Created by Richard Sugiharto on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StockBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
